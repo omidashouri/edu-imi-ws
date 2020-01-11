5d0e3d1f312df22002cd8a50520f3bfb83ea6a74
o.ashouri <omidashouri@gmail.com>
2020-01-11 13:38:51 +0330

    imi education webservice project : omidashouri : 98.10.21

---
+++ edu-imi-web/src/main/java/edu/imi/ir/eduimiws/EduImiWsApplication.java
@@ -0,0 +1,13 @@
+package edu.imi.ir.eduimiws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduImiWsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduImiWsApplication.class, args);
+    }
+
+}
---
+++ edu-imi-web/src/test/java/edu/imi/ir/eduimiws/EduImiWsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.imi.ir.eduimiws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduImiWsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
