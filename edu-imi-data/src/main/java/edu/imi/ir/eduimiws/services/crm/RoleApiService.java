43add0f6dd337f94da955f9723cd4f5582939a57
Omid Ashouri <omidashouri@gmail.com>
2020-05-09 00:52:53 +0430

    Role Privileges + part four  : omidashouri : 99.02.20

--- edu-imi-data/src/main/java/edu/imi/ir/eduimiws/services/crm/RoleApiService.java
+++ edu-imi-data/src/main/java/edu/imi/ir/eduimiws/services/crm/RoleApiService.java
@@ -2,6 +2,8 @@ package edu.imi.ir.eduimiws.services.crm;
 
 import edu.imi.ir.eduimiws.domain.crm.PrivilegeApiEntity;
 import edu.imi.ir.eduimiws.domain.crm.RoleApiEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.Collection;
 
@@ -14,4 +16,8 @@ public interface RoleApiService {
     Collection<RoleApiEntity> findAllByRoleName(String roleName);
 
     Collection<RoleApiEntity> findAllByRoleNameAndPrivileges(String roleName,Collection<PrivilegeApiEntity> privileges);
+
+    Page<RoleApiEntity> findAllRoleEntityPagesOrderByCreateDateDesc(Pageable pageable);
+
+    RoleApiEntity findByRolePublicId(String rolePublicId);
 }
