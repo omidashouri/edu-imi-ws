9cc42804c910173148a4db055512f80eec6a0cf7
o.ashouri <omidashouri@gmail.com>
2021-04-26 08:11:22 +0430

    add actuator permission: omidashouri :  00.02.06

--- edu-imi-web/src/main/java/edu/imi/ir/eduimiws/security/RootUrlSecurity.java
+++ edu-imi-web/src/main/java/edu/imi/ir/eduimiws/security/RootUrlSecurity.java
@@ -15,6 +15,7 @@ public class RootUrlSecurity extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("**/swagger-ui/**","/swagger-ui/**","/v3/api-docs/**","/v3/api-docs","/v2/api-docs/**","/configuration/**","/swagger*/**","/webjars/**").permitAll()
                 .antMatchers(HttpMethod.POST,"**/afterPaymentResponse").permitAll()
+//                .antMatchers("/actuator/**").permitAll()
                 .anyRequest().authenticated()
         ;
     }
