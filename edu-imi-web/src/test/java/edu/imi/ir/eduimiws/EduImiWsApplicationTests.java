4e2b89f2bfd0dc44ff292113ae88558c783429d6
omidashouri <omidashouri@gmail.com>
2021-05-10 10:41:18 +0430

    ass basic test  : omidashouri :  00.02.20

--- edu-imi-web/src/test/java/edu/imi/ir/eduimiws/EduImiWsApplicationTests.java
+++ edu-imi-web/src/test/java/edu/imi/ir/eduimiws/EduImiWsApplicationTests.java
@@ -1,11 +1,46 @@
 package edu.imi.ir.eduimiws;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.imi.ir.eduimiws.models.request.UserLoginRequestModel;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("dev")
 class EduImiWsApplicationTests {
 
+    @LocalServerPort
+    int randomServerPort;
+
+
+    private final RestTemplate restTemplate =  new RestTemplate();
+
+    MockRestServiceServer mockServer =
+            MockRestServiceServer.bindTo(restTemplate).build();
+
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+
+    @Before
+    public void setUp() throws Exception {
+
+        String detailsString = objectMapper
+                .writeValueAsString(new UserLoginRequestModel("9057", "", null));
+
+        mockServer.verify();
+    }
+
+    @Ignore
     @Test
     void contextLoads() {
     }
