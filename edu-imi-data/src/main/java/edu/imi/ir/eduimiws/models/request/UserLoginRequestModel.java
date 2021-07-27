a4ccc146cafa3201baefd499326f054f7130a4ae
omidashouri <omidashouri@gmail.com>
2021-07-27 10:41:24 +0430

    refactor UserLoginRequestModel   : omidashouri :  00.05.05

--- edu-imi-data/src/main/java/edu/imi/ir/eduimiws/models/request/UserLoginRequestModel.java
+++ edu-imi-data/src/main/java/edu/imi/ir/eduimiws/models/request/UserLoginRequestModel.java
@@ -1,20 +1,28 @@
 package edu.imi.ir.eduimiws.models.request;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonRootName;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
 
+@Schema(name = "Login", description = "Login Information for Authentication")
+@EqualsAndHashCode(callSuper = false)
+@JsonRootName(value = "Login")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserLoginRequestModel {
 
+
 //    create this for user sign in request
 
+    @Schema(title = "UserName", maxLength=10, example = " ", required = true)
     private String username;
+
+    @Schema(title = "PassWord", maxLength=10, example = " ", required = true)
     private String password;
+
+    @Schema(title = "role", maxLength=10, example = " ")
     private String role;
 }
