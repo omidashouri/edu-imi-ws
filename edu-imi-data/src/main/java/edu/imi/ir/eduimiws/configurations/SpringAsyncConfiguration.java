38a68035a8f09518b8fad84b5a58a806425a9522
omidashouri <omidashouri@gmail.com>
2021-09-28 22:11:58 +0330

    add spring ssl : omidashouri : 00.07.06

--- edu-imi-data/src/main/java/edu/imi/ir/eduimiws/configurations/SpringAsyncConfiguration.java
+++ edu-imi-data/src/main/java/edu/imi/ir/eduimiws/configurations/SpringAsyncConfiguration.java
@@ -1,26 +1,24 @@
 package edu.imi.ir.eduimiws.configurations;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.servlet.TomcatServletWebServerFactoryCustomizer;
 import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.servlet.DispatcherServlet;
 
 import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 
 @Configuration
@@ -79,12 +77,29 @@ public class SpringAsyncConfiguration implements AsyncConfigurer {
     }*/
 
 
+    /*    @Bean
+    public TomcatServletWebServerFactory tomcatServletWebServerFactory() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory(){
+        };
+            tomcat.addAdditionalTomcatConnectors(connector());
+        return tomcat;
+    }*/
 
 
     @Bean
-    public TomcatServletWebServerFactory tomcatServletWebServerFactory() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-            tomcat.addAdditionalTomcatConnectors(connector());
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                var securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                var collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(connector());
         return tomcat;
     }
 
@@ -94,6 +109,10 @@ public class SpringAsyncConfiguration implements AsyncConfigurer {
         connector.setAsyncTimeout(180000);
         connector.setPort(Integer.valueOf(8083));
         connector.setAllowTrace(true);
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);
         return connector;
     }
 
