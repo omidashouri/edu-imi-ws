9e876da47073d9b2e9a9dade77731e02aa2645fb
omidashouri <omidashouri@gmail.com>
2021-12-26 10:25:25 +0330

    move ssl for dev profile to new configuration class : omidashouri : 001005

--- edu-imi-data/src/main/java/edu/imi/ir/eduimiws/configurations/SpringAsyncConfiguration.java
+++ edu-imi-data/src/main/java/edu/imi/ir/eduimiws/configurations/SpringAsyncConfiguration.java
@@ -1,15 +1,8 @@
 package edu.imi.ir.eduimiws.configurations;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.catalina.Context;
-import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
-import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -84,42 +77,5 @@ public class SpringAsyncConfiguration implements AsyncConfigurer {
             tomcat.addAdditionalTomcatConnectors(connector());
         return tomcat;
     }*/
-
-
-//    SSL configuration
-    @Bean
-    public ServletWebServerFactory servletContainer() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-            @Override
-            protected void postProcessContext(Context context) {
-                var securityConstraint = new SecurityConstraint();
-                securityConstraint.setUserConstraint("CONFIDENTIAL");
-                var collection = new SecurityCollection();
-                collection.addPattern("/*");
-                securityConstraint.addCollection(collection);
-                context.addConstraint(securityConstraint);
-            }
-        };
-        tomcat.addAdditionalTomcatConnectors(connector());
-        return tomcat;
-    }
-
-    @Bean
-    public Connector connector(){
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setAsyncTimeout(180000);
-        connector.setPort(Integer.valueOf(8083));
-        connector.setAllowTrace(true);
-        connector.setScheme("http");
-        connector.setPort(8080);
-        connector.setSecure(false);
-        connector.setRedirectPort(8443);
-        return connector;
-    }
-
-    @Bean
-    public TomcatConnectorCustomizer tomcatConnectorCustomizer(){
-        return connector -> {this.connector();};
-    }
 
 }
