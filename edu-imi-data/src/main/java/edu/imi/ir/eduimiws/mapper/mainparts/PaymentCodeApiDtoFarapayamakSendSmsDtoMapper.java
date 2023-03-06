ba23db50b34eab32789f60078e7c713e16729407
D.Sharifi <sharifi.delaram@gmail.com>
2023-03-06 23:48:34 +0330

    farapayamak correct package name:d.sharifi:14011215

--- edu-imi-data/src/main/java/edu/imi/ir/eduimiws/mapper/mainparts/PaymentCodeApiDtoFarapayamakSendSmsDtoMapper.java
+++ edu-imi-data/src/main/java/edu/imi/ir/eduimiws/mapper/mainparts/PaymentCodeApiDtoFarapayamakSendSmsDtoMapper.java
@@ -2,7 +2,7 @@ package edu.imi.ir.eduimiws.mapper.mainparts;
 
 
 import edu.imi.ir.eduimiws.mapper.MappingUtil;
-import edu.imi.ir.eduimiws.models.dto.crm.FarapayamakSendSmsDto;
+import edu.imi.ir.eduimiws.models.dto.crm.farapayamak.FarapayamakSendSmsDto;
 import edu.imi.ir.eduimiws.models.dto.mainparts.PaymentCodeApiDto;
 import edu.imi.ir.eduimiws.security.FarapayamakCredential;
 import edu.imi.ir.eduimiws.utilities.CommonUtils;
